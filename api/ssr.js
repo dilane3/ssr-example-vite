28b83b502a28cd24cb54baac3e01a1b1117e0228
dilane3 <komboudilane125@gmail.com>
2023-10-14 15:02:58 +0100

    update

--- api/ssr.js
+++ api/ssr.js
@@ -3,16 +3,20 @@ import fs from "node:fs/promises";
 // Create server for production only
 const handler = async (req, res) => {
   try {
-    console.log(req);
     const url = req.url;
 
     let template;
     let render;
 
-    // Always read fresh template in development
-    template = await fs.readFile("client/index.html", "utf-8");
+    // get the list of files from the current directory
+    const files = await fs.readdir("./dist/server");
 
-    render = (await import("server/entry-server.js")).render;
+    console.log(files);
+
+    // Always read fresh template in development
+    template = await fs.readFile("./dist/client/index.html", "utf-8");
+
+    render = (await import("./dist/server/entry-server.js")).render;
 
     const rendered = await render(url);
 

b2247d4a151c12db7034871d2dbe572197e3827a
dilane3 <komboudilane125@gmail.com>
2023-10-14 16:04:48 +0100

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,38 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const [counter, setCounter] = React.useState(0);
+  const [value, setValue] = React.useState("");
+
+  const increment = () => {
+    setCounter(counter + + value);
+  };
+
+  const decrement = () => {
+    setCounter(counter - + value);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (!isNaN(Number(value))) {
+      setValue(value);
+    }
+  };
+
   return (
     <div style={{ display: "block" }}>
       <h1>Home</h1>
       <p>This is the home page.</p>
+
+      <input type="text" value={value} onChange={handleChange} />
+
+      <div style={{ display: "flex", padding: "20px", alignItems: "center", gap: "20px" }}>
+        <button onClick={decrement}>-</button>
+        <span>{counter}</span>
+        <button onClick={increment}>+</button>
+      </div>
 
       <div>
         <Link to="/setting">Go to setting page</Link>
